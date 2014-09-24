f5a8584973948a048bfb3072b51c26181b94fd12
tli123 <tommyli12345@gmail.com>
2014-09-24 08:35:52 -0400

    Updated with class lab

--- 05-stringStuff/Driver.java
+++ 05-stringStuff/Driver.java
@@ -4,5 +4,9 @@ public class Driver {
 	System.out.println(s.diff21(14));
 	System.out.println(s.makeAbba("OMG", "HI"));
 	System.out.println(s.nonStart("Hello", "there"));
+	System.out.println(s.nearHundred(91));
+	System.out.println(s.mixStart("1ix snacks"));
+	System.out.println(s.teaParty(20, 8));
+	System.out.println(s.lastDigit(12, 13, 22));
     }
 }
--- 05-stringStuff/stringStuff.java
+++ 05-stringStuff/stringStuff.java
@@ -5,12 +5,35 @@ public class stringStuff {
     public String nonStart(String a, String b) {
 	return a.substring(1) + b.substring(1);
     }
+    //Working
     public int diff21(int n) {
 	int diff = Math.abs(n - 21);
 	if (n > 21) {
 	    return diff * 2;
 	}
 	return diff;
+    }
+    //working
+    public int teaParty(int tea, int candy) {
+	if ((tea < 5) || (candy < 5)) {
+    return 0;
+	}
+	if ((tea >= candy * 2) || (candy >= tea * 2)) {
+	    return 2;
+	}
+	return 1;
+    }
+    //working
+    public boolean lastDigit(int a, int b, int c) {
+	return (a % 10 == b % 10) || (a % 10 == c % 10) || (b % 10 == c %10);
+    }
+    //working
+    public boolean nearHundred(int n) {
+	return ((Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10));
+    }
+    //working
+    public boolean mixStart(String str) {
+	return (str.indexOf("ix") >= 0);
     }
 }
 
