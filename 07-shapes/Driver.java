d484dd04326af3b18aa4e03347242a7b8af1ea91
Tommy Li <tommyli12345@gmail.com>
2014-10-01 12:48:30 -0400

    Fun with shapes!

---
+++ 07-shapes/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args) {
+	boxfunc x = new boxfunc();
+	System.out.println(x.box(7, 2));
+	System.out.println(x.Tri1(8));
+	System.out.println(x.Tri2(12));
+    }
+}
---
+++ 07-shapes/boxfunc.java
@@ -0,0 +1,50 @@
+public class boxfunc {
+    public String box(int a, int b){
+	int cc = 0;
+	String boxy = "";
+	while (cc < b){
+	    int rc = 0;
+	    while (rc < a) {
+		boxy = boxy + "*";
+		rc = rc + 1;
+	    }
+	    boxy = boxy + "\n";
+	    cc = cc + 1;
+	}
+	return boxy;
+    }
+    public String Tri1(int a) {
+	int counter = 1;
+	String res = "";
+	while (counter <= a){
+	    int c = 1;
+	    while (c < counter){
+		res = res + "*";
+		c = c + 1;
+	    }
+	    res = res + "\n";
+	    counter = counter + 1;
+	}
+	return res;
+    }
+    public String Tri2(int a) {
+	int counter = 1;
+	String res = "";
+	while (counter <= a){
+	    int c = 0;
+	    int diff = (a - counter);
+	    while (c < diff){
+		res = res + " ";
+		c = c + 1;
+	    }
+	    int d = 1;
+	    while (d <= counter) {
+		res = res + "*";
+		d = d + 1;
+	    }
+	    res = res + "\n";
+	    counter = counter + 1;
+	}
+	return res;
+    }
+}
