927dabe8a827dc61d22ea820771fca5a28c1b970
tli123 <tommyli12345@gmail.com>
2014-10-03 08:38:50 -0400

    Framin' ~~

---
+++ 08-for/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args){
+	framer x = new framer();
+	System.out.println(x.frame(3, 5));
+	System.out.println(x.stringSplosion("Computers"));
+	System.out.println(x.stringX("xHIx OMG"));
+    }
+}
