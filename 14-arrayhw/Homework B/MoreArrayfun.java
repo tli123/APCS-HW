000e140a158bdbec335e4f7f2590ff807e1806e2
tli123 <tommyli12345@gmail.com>
2014-10-30 08:14:17 -0400

    Fixed canBalance()

--- 14-arrayhw/Homework B/MoreArrayfun.java
+++ 14-arrayhw/Homework B/MoreArrayfun.java
@@ -107,15 +107,11 @@ public class MoreArrayfun {
 
     public boolean canBalance() {
 	int sum1 = 0;
-	int sum2 = 0;
 	for (int i = 0; i < nums.length; i++) {
-	    for (int j = 0; j < nums.length; j++) {
-		if (j > i) {
-		    sum1 += nums[j];
-		}
-		else {
-		    sum2 += nums[j];
-		}
+	    int sum2 = 0;
+	    sum1 += nums[i];
+	    for (int j = i+1; j < nums.length; j++) {
+		sum2 += nums[j];
 	    }
 	    if (sum1 == sum2) {
 		return true;
