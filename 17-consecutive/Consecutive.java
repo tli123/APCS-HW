ecf9374c644e23b710eff16853049d550fd1a67c
Tommy Li <tommyli12345@gmail.com>
2014-11-13 16:21:00 -0500

    FInished HW 17

--- 17-consecutive/Consecutive.java
+++ 17-consecutive/Consecutive.java
@@ -3,28 +3,53 @@ import java.io.*;
 
 public class Consecutive {
 
-    private int[] nums;
-    private ArrayList<Integer> num;
-    private ArrayList<Integer> temp;
-
-    public Consecutive(int n) {
-	nums = new int[n + 1];
-	for (int i = 0; i <= n; i++) {
-	    nums[i] = i;
-	    num.add(i);
+    public ArrayList Randomnize(ArrayList<Integer> num) {
+	Random r = new Random();
+	ArrayList<Integer> temp = new ArrayList<Integer>();
+	for (int i = 0; i < 21; i++) {
+	    int rand = r.nextInt(num.size());
+	    temp.add(num.get(rand));
+	    num.remove(rand);
 	}
+	return temp;
+    }
+
+    public String Randomnize(int[] nums) {
+	Random r = new Random();
+	int[] temp = new int[nums.length];
+	for (int i = 0; i < 21; i++) {
+	    int rand = r.nextInt(nums.length);
+	    temp[i] = nums[rand];
+	    int k = 0;
+	    int[] temp2 = new int[nums.length - 1];
+	    for (int j = 0; j < nums.length; j++) {
+		if (j != rand) {
+		    temp2[k] = nums[j];
+		    k++;
+		}
+	    }
+	    nums = temp2;
+	}
+	return toString(temp);
     }
     
-    public void SpaceEfficient() {
-	for (int i = 0; i < num.size(); i++) {
-	    int x = (int)(Math.random() * num.size() + 1);
-	    temp.add(num.get(x));
+    public String toString(int[] nums) {
+	String Arraystring = "{";
+	for (int i = 0; i < nums.length - 1; i++) {
+	    Arraystring += nums[i] + ", ";
 	}
+	return Arraystring += nums[nums.length - 1] + "}";
     }
 
     public static void main(String[] args) {
-	Consecutive c = new Consecutive(10);
-	c.SpaceEfficient();
-	System.out.println(c);
+	Consecutive a = new Consecutive();
+	ArrayList<Integer> c = new ArrayList<Integer>();
+	int[] nums = new int[21];
+	for (int i = 0; i <= 20; i++) {
+	    nums[i] = i;
+	    c.add(i);
+	}
+	System.out.println(a.Randomnize(c));
+	System.out.println(a.Randomnize(nums));
     }
 }
