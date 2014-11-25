a1ecf372fd235a33a9afddc019056702a0445fdb
Tommy Li <tommyli12345@gmail.com>
2014-11-24 23:25:13 -0500

    Wordsearch complete!

--- 18-wordsearch/WordSearch.java
+++ 18-wordsearch/WordSearch.java
@@ -1,83 +1,25 @@
 import java.util.*;
+import java.io.*;
 
-
-/**
- * Creates a word search puzzle
- *
- */
-public class WordSearch{
+public class WordSearch {
 
     private char[][] board;
-
-    public WordSearch(int r, int c){
-	board = new char[r][c];
+    private String words = "Wordlist:\n";
+    
+    public WordSearch(int row, int col) {
+	board = new char[row][col];
 	for (int i = 0; i < board.length; i++) {
 	    for (int j = 0; j < board[i].length; j++) {
-		board[i][j]='.';
+		board[i][j] = '.';
 	    }
 	}
     }
 
-    public WordSearch(){
-	this(20,30);
+    public WordSearch() {
+	this(20, 30);
     }
 
-    public boolean AddWord(String w) {
-	try {
-	    String[] methods = new String[8];
-	    methods[0] = "H";
-	    methods[1] = "B";
-	    methods[2] = "V";
-	    methods[3] = "VB";
-	    methods[4] = "DL";
-	    methods[5] = "DLB";
-	    methods[6] = "DR";
-	    methods[7] = "DRB";
-	    Random r = new Random();
-	    Random x = new Random();
-	    Random y = new Random();
-	    String method = methods[r.nextInt(methods.length - 1)];
-	    int row = x.nextInt(board.length);
-	    int col = y.nextInt(board[0].length);
-	    if (method == "H") {
-		addWordH(w, row, col);
-		return true;
-	    }
-	    if (method == "B") {
-		addWordB(w, row, col);
-		return true;
-	    }
-	    if (method == "V") {
-		addWordV(w, row, col);
-		return true;
-	    }
-	    if (method == "VB") {
-		addWordVB(w, row, col);
-		return true;
-	    }
-	    if (method == "DR") {
-		addWordDR(w, row, col);
-		return true;
-	    }
-	    if (method == "DRB") {
-		addWordDRB(w, row, col);
-		return true;
-	    }
-	    if (method == "DLB") {
-		addWordDLB(w, row, col);
-		return true;
-	    }
-	    if (method == "DL") {
-		addWordDL(w, row, col);
-		return true;
-	    }
-	} catch (Exception e) {
-	    return false;
-	}
-	return false;
-    }
-
-    public String toString(){
+    public String toString() {	
 	String s = "";
 	for (int i = 0; i < board.length; i++) {
 	    for (int j = 0; j < board[i].length; j++) {
@@ -88,137 +30,218 @@ public class WordSearch{
 	return s;
     }
 
-    public void addWordH(String w,int row, int col){
-	if (row <= board.length && col <= board[1].length) {
-	    int r = row, c = col;
+    public boolean Overflow(String word, int row, int col, String type) {
+	if (row < board.length && col < board[0].length) {
 	    boolean add = true;
-	    if (col + w.length() > board[1].length) {
-		add = false;
+	    if (type.equals("h")) {
+		if (col + word.length() > board[0].length) {
+		    add = false;
+		}
+		if (add == true) {
+		    for (int i = 0; i < word.length(); i++) {
+			if (board[row][col+i] != '.') {
+			    add = false;
+			    break;
+			}
+		    }
+		}
 	    }
-	    if (add == true) {
-		for (int i = 0; i < w.length(); i++) {
-		    if (board[r][c+i] != '.') {
+	    if (type.equals("v")) {
+		if (row + word.length() > board.length) {
+		    add = false;
+		}
+		if (add == true) {
+		    for (int i = 0; i < word.length(); i++) {
+			if (board[row+i][col] != '.') {
+			    add = false;
+			    break;
+			}
+		    }
+		}
+	    }
+	    if (type.equals("rd")) {
+		if (row + word.length() > board.length) {
+		    add = false;
+		}
+		if (add == true) {
+		    for (int i = 0; i < word.length(); i++) {
+			if (board[row+i][col+i] != '.'){
+			    add = false;
+			    break;
+			}
+		    }
+		}
+	    }
+	    if (type.equals("ld")) {
+		if (row + word.length() > board.length) {
+		    add = false;
+		}
+		for (int i = 0; i < word.length(); i++) {
+		    if (board[row-i][col-i] != '.') {
 			add = false;
 			break;
 		    }
 		}
 	    }
-	    if (add == true) {
-		for (int i=0;i<w.length();i++){
-		    board[r][c+i] = w.charAt(i);
-		}
+	    return add;
+	}
+	return false;
+    }
+
+    public void Horizontal(String word, int row, int col, char type) {
+	boolean add = Overflow(word, row, col, "h");
+	if (add == true) {
+	    if (type == 'f') {
+		Wordlist(word);
+	    }
+	    for (int i = 0; i < word.length(); i++) {
+		board[row][col+i] = word.charAt(i);
 	    }
 	}
     }
 
-    public void addWordB(String w,int row, int col) {
-	String word = new StringBuilder(w).reverse().toString();
-	addWordH(word, row, col);
+    public void BHorizontal(String word, int row, int col) {
+	String w = new StringBuilder(word).reverse().toString();
+	Wordlist(word);
+	Horizontal(w, row, col, 'b');
     }
 
-    public void addWordV(String w, int row, int col) {
-	if (row <= board.length && col <= board[1].length) {
-	    int r = row, c = col;
-	    boolean add = true;
-	    if (row + w.length() > board.length) {
-		add = false;
+    public void Vertical(String word, int row, int col, char type) {
+	boolean add = Overflow(word, row, col, "v");
+	if (add == true) {
+	    if (type == 'f') {
+		Wordlist(word);
 	    }
-	    if (add == true) {
-		for (int i = 0; i < w.length(); i++) {
-		    if (board[r+i][c] != '.') {
-			add = false;
-			break;
-		    }
-		}
-	    }
-	    if (add == true) {
-		for (int i=0;i<w.length();i++){
-		    board[r+i][c] = w.charAt(i);
-		}
+	    for (int i = 0; i < word.length(); i++) {
+		board[row+i][col] = word.charAt(i);
 	    }
 	}
     }
 
-    public void addWordVB(String w, int row, int col) {
-	String word = new StringBuilder(w).reverse().toString();
-	addWordV(word, row, col);
+    public void BVertical(String word, int row, int col) {
+	String w = new StringBuilder(word).reverse().toString();
+	Wordlist(word);
+	Vertical(w, row, col, 'b');
     }
 
-    public void addWordDL(String w, int row, int col) {
-	if (row <= board.length && col <= board[1].length) {
-	    int r = row, c = col;
-	    boolean add = true;
-	    if (row + w.length() > board.length) {
-		add = false;
+    public void RDiagonal(String word, int row, int col, char type) {
+	boolean add = Overflow(word, row, col, "rd");
+	if (add == true) {
+	    if (type == 'f') {
+		Wordlist(word);
 	    }
-	    if (add == true) {
-		for (int i = 0; i < w.length(); i++) {
-		    if (board[r+i][c+i] != '.') {
-			add = false;
-			break;
-		    }
-		}
-	    }
-	    if (add == true) {
-		for (int i=0;i<w.length();i++){
-		    board[r+i][c+i] = w.charAt(i);
-		}
+	    for (int i = 0; i < word.length(); i++) {
+		board[row+i][col+i] = word.charAt(i);
 	    }
 	}
     }
 
-    public void addWordDLB(String w, int row, int col) {
-	String word = new StringBuilder(w).reverse().toString();
-	addWordDL(word, row, col);
+    public void RBDiagonal(String word, int row, int col) {
+	String w = new StringBuilder(word).reverse().toString();
+	Wordlist(word);
+	RDiagonal(w, row, col, 'b');
     }
 
-    public void addWordDR(String w, int row, int col) {
-	if (row <= board.length && col <= board[1].length) {
-	    int r = row, c = col;
-	    boolean add = true;
-	    if (row + w.length() > board.length) {
-		add = false;
+    public void LDiagonal(String word, int row, int col, char type) {
+	boolean add = Overflow(word, row, col, "ld");
+	if (add == true) {
+	    if (type == 'f') {
+		Wordlist(word);
 	    }
-	    if (add == true) {
-		for (int i = 0; i < w.length(); i++) {
-		    if (board[r+i][c-i] != '.') {
-			add = false;
-			break;
-		    }
-		}
-	    }
-	    if (add == true) {
-		for (int i=0;i<w.length();i++){
-		    board[r+i][c-i] = w.charAt(i);
-		}
+	    for (int i = 0; i < word.length(); i++) {
+		board[row-i][col-i] = word.charAt(i);
 	    }
 	}
     }
 
-    public void addWordDRB(String w, int row, int col) {
-	String word = new StringBuilder(w).reverse().toString();
-	addWordDR(word, row, col);
+    public void LBDiagonal(String word, int row, int col) {
+	String w = new StringBuilder(word).reverse().toString();
+	Wordlist(word);
+	LDiagonal(w, row, col, 'b');
+    }
+
+    public boolean addWord(String w) {
+	try {
+	    String[] methods = new String[]{"H", "HB", "V", "VB", "RD", "RDB", "LD", "LDB"};
+	    Random r = new Random();
+	    Random x = new Random();
+	    Random y = new Random();
+	    String method = methods[r.nextInt(methods.length - 1)];
+	    int row = x.nextInt(board.length - 1);
+	    int col = y.nextInt(board[0].length - 1);
+	    if (method.equals("H")) {
+		Horizontal(w, row, col, 'f');
+		return true;
+	    }
+	    if (method.equals("HB")) {
+		BHorizontal(w, row, col);
+		return true;
+	    }
+	    if (method.equals("V")) {
+		Vertical(w, row, col, 'f');
+		return true;
+	    }
+	    if (method.equals("VB")) {
+		BVertical(w, row, col);
+		return true;
+	    }
+	    if (method.equals("RD")) {
+		RDiagonal(w, row, col, 'f');
+		return true;
+	    }
+	    if (method.equals("RDB")) {
+		RBDiagonal(w, row, col);
+		return true;
+	    }
+	    if (method.equals("LD")) {
+		LDiagonal(w, row, col, 'f');
+		return true;
+	    }
+	    if (method.equals("LDB")) {
+		LBDiagonal(w, row, col);
+		return true;
+	    }
+	} catch (Exception e) {
+	    System.out.println("Error: " + e);
+	}
+	return false;	
     }
 
     public void populate() {
-	//populates wordlist after words added
+	char[] alpha = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	Random r = new Random();
+	for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j < board[i].length; j++) {
+		if (board[i][j] == '.') {
+		    board[i][j] = alpha[r.nextInt(alpha.length - 1)];
+		}
+	    }
+	}
     }
 
-    public void Wordlist() {
-	wordlist is added wit hhe words
+    public void Wordlist(String w) {
+	words = words + w + "\n";
     }
 
-    public static void main(String[] args) {
+    public String Wordlist() {
+	return words;
+    }
+
+    public static void main(String args[]) {
 	WordSearch w = new WordSearch();
-	System.out.println(w);
-	w.AddWord("david");
-	w.addWordH("hello",3,5);
-	w.addWordH("look",3,8);	
-	w.addWordB("Edward", 6, 4);
-	w.addWordV("andysucks", 7, 2);
-	w.addWordVB("omghai", 7, 3);
-	w.addWordDL("lolxd", 3, 15);
-	w.addWordDR("rofl", 1, 20);
-	System.out.println(w);
+	System.out.println("Before:\n" + w);
+	w.Horizontal("hello", 1, 1, 'f');
+	w.BHorizontal("look", 1, 7);
+	w.Vertical("computer", 1, 6, 'f');
+	w.BVertical("laptop", 3, 3);
+	w.RDiagonal("word", 6, 8, 'f');
+	w.RBDiagonal("science", 9, 10);
+	w.LDiagonal("desktop", 20, 20, 'f');
+	w.LBDiagonal("orange", 20, 13);
+	w.addWord("apple");
+	System.out.println("After adding words:\n" + w);
+	w.populate();
+	System.out.println("Completed puzzle:\n" + w);
+	System.out.println(w.Wordlist());
     }
 }
