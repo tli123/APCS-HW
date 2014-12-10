2817f7ac3fec51de6e5680ebdd55b9f940a5b720
tli123 <tommyli12345@gmail.com>
2014-12-10 08:41:23 -0500

    Added interfaces

--- 12-stuyablo/Monster.java
+++ 12-stuyablo/Monster.java
@@ -1,9 +1,16 @@
-public class Monster extends Basechar{
+public class Monster extends Basechar implements Comparable{
     int healthboost=(int)Math.ceil(Math.random()*400);
     public Monster (String n) {
 	super(n);
 	this.health = this.health + healthboost;
 	this.attack = this.attack + (int)Math.ceil(Math.random()*5);
+    }
+
+    public String Comparable (Monster other) {
+	if (other.name.compareTo(this.name) > 0) {
+	    return other.name;
+	}
+	return this.name;
     }
     /*
       public void Attack (Basechar other) {
