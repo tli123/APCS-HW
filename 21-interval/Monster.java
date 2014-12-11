9bc862a47fb432aa54cc9793894982bab45c8dd2
Tommy Li <tommyli12345@gmail.com>
2014-12-11 00:28:05 -0500

    Tried to finish

--- 21-interval/Monster.java
+++ 21-interval/Monster.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class Monster extends Basechar implements Comparable{
     int healthboost=(int)Math.ceil(Math.random()*400);
     public Monster (String n) {
@@ -9,6 +12,48 @@ public class Monster extends Basechar implements Comparable{
     public int compareTo(Object other) {
 	Monster o = (Monster)other;
 	return (int)this.getName().charAt(0) - (int)o.getName().charAt(0);  
+    }					      
+
+    public static void main(String[] args) {
+	/*
+	  Does not compile
+	  1. Basechar.toString is not appliable, actual + formal arguements
+	     differ in length
+	  2. .size has private access to ArrayList
+	  Note: Monster.java uses unchecked or unsafe operations.
+	  Note: Recompile with -Xlinr:unchecked for details
+	 */
+	
+        ArrayList<String> names = new ArrayList<String>();
+	names.add("Teacher");
+	names.add("Student");
+	names.add("Grinch");
+	names.add("Wolf");
+	names.add("Mafia");
+        ArrayList<Monster> monsters = new ArrayList<Monster>();
+	for (int i = 0; i < 5; i++) {
+	    Random r = new Random();
+	    int pos = r.nextInt(5);
+	    Monster a = new Monster(names.get(pos));
+	    monsters.add(a);
+	    names.remove(pos);
+	}
+
+	String s = "[";
+	for (int i = 0; i < monsters.size - 1; i++){
+	    s = s + monsters.get(i) + ", ";
+	}
+	return s + monsters.get(4) + "]";
+	System.out.println(s);
+	Collections.sort(monsters);
+
+	String x = "[";
+	for (int i = 0; i < monsters.size - 1; i++){
+	    x = x + monsters.get(i) + ", ";
+	}
+	return x + monsters.get(4) + "]";
+	System.out.println(toString(x));
+	
     }
 
     /*
